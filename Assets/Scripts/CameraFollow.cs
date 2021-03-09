0c5298e6f6cd56595a2423fae3cb5acd5734180f
Dzalapino <224407@edu.p.lodz.pl>
2021-03-09 03:05:35 +0100

    Implemented camera script with zooming dependent on distance between players

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -1,18 +1,33 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Rendering;
 
+// Script is responsible for camera movement and both players staing in the viewport
 public class CameraFollow : MonoBehaviour
 {
-    public GameObject ObjectA;
-    public GameObject ObjectB;
-    public Vector3 BetweenPlayers;
+    public GameObject PlayerOne;
+    public GameObject PlayerTwo;
+    public float MinY, MaxY; // Minimal and maximal zoom of the camera (MinY - nearest, MaxY - farthest distance on vertical axis)
+    public float Multiplier; // How fast the zooming will occur
+    private Vector3 pointBetweenPlayers;
+    private float distanceBetweenPlayers;
 
-    // Update is called once per frame
+    // Updating position of the camera in the center between both players
     void LateUpdate()
     {
-        BetweenPlayers.x = (ObjectA.transform.position.x + ObjectB.transform.position.x) / 2;
-        BetweenPlayers.z = (ObjectA.transform.position.x + ObjectB.transform.position.x) / 2;
-        transform.position = BetweenPlayers;
+        // Calculating distance between players
+        distanceBetweenPlayers = Mathf.Sqrt(
+            (Mathf.Pow(PlayerTwo.transform.position.x - PlayerOne.transform.position.x, 2))
+            + (Mathf.Pow(PlayerTwo.transform.position.z - PlayerOne.transform.position.z, 2)));
+        
+        // Calculating center point and zoom (point on y axis, depends on distance between players)
+        pointBetweenPlayers.x = (PlayerOne.transform.position.x + PlayerTwo.transform.position.x) / 2;
+        pointBetweenPlayers.y = Mathf.Clamp(distanceBetweenPlayers * Multiplier, MinY, MaxY);
+        pointBetweenPlayers.z = (PlayerOne.transform.position.z + PlayerTwo.transform.position.z) / 2;
+        
+        // Assigning vector to the camera position
+        transform.position = pointBetweenPlayers;
     }
 }
