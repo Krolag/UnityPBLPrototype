321f325e21238aaf9544be363e058c6a2ae0db8a
WDobies <68401136+WDobies@users.noreply.github.com>
2021-03-13 18:06:06 +0100

    Fixed collecting sprites for two players

--- Assets/Scripts/SpriteMagnet.cs
+++ Assets/Scripts/SpriteMagnet.cs
@@ -7,9 +7,9 @@ public class SpriteMagnet : MonoBehaviour
     private float _range = 4;
     void Update()
     {
-        var player = GameObject.FindGameObjectWithTag("Player");
+        var players = GameObject.FindGameObjectsWithTag("Player");
 
-        if (player != null)
+        foreach(GameObject player in players)
         {
             var acctualDistance = Vector3.Distance(player.transform.position, transform.position); //distance between player and sprite
 
