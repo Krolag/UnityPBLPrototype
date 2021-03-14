9c2e66b8034e62da38ff683e099cdc98f5d51625
Dzalapino <224407@edu.p.lodz.pl>
2021-03-14 03:16:04 +0100

    Camera movement done, players are now staying in the viewport, just switch camera colliders mesh rendering.

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -8,7 +8,8 @@ using UnityEngine.Rendering;
 [RequireComponent(typeof(Camera))]
 public class CameraFollow : MonoBehaviour
 {
-    public Transform PlayerOne, PlayerTwo;
+    public Transform PlayerOne, PlayerTwo, CameraTransform;
+    public GameObject LeftCollider, RightCollider, TopCollider, BottomCollider;
     public Vector3 Offset; // Adds position to the center point
     public float SmoothAmount; // How smooth camera moves
     public float MinZoom, MaxZoom; 
@@ -21,7 +22,29 @@ public class CameraFollow : MonoBehaviour
     {
         camera = GetComponent<Camera>();
     }
-    
+
+    void Update()
+    {
+        // Viewport to world cords mapping
+        // 10 in he third function attribute is the distance between camera and players in the Y axis
+        Vector3 cameraBottomLeft = camera.ViewportToWorldPoint(new Vector3(0.0f, 0.0f, 10.0f));
+        Vector3 cameraCenter = camera.ViewportToWorldPoint(new Vector3(0.5f, 0.5f, 10.0f));
+        Vector3 cameraTopRight = camera.ViewportToWorldPoint(new Vector3(1.0f, 1.0f, 10.0f));
+        
+        // Updating scale and position of camera border colliders
+        LeftCollider.transform.position = new Vector3(cameraBottomLeft.x, 0f, cameraCenter.z);
+        LeftCollider.transform.localScale = new Vector3(0.5f, 1f, cameraTopRight.z - cameraBottomLeft.z);
+        
+        RightCollider.transform.position = new Vector3(cameraTopRight.x, 0f, cameraCenter.z);
+        RightCollider.transform.localScale = new Vector3(0.5f, 1f, cameraTopRight.z - cameraBottomLeft.z);
+        
+        TopCollider.transform.position = new Vector3(cameraCenter.x, 0f, cameraTopRight.z);
+        TopCollider.transform.localScale = new Vector3(cameraTopRight.x - cameraBottomLeft.x, 1f, 0.5f);
+
+        BottomCollider.transform.position = new Vector3(cameraCenter.x, 0f, cameraBottomLeft.z);
+        BottomCollider.transform.localScale = new Vector3(cameraTopRight.x - cameraBottomLeft.x, 1f, 0.5f);
+    }
+
     // Updating position of the camera in the center between both players and zooming
     void LateUpdate()
     {
@@ -30,16 +53,15 @@ public class CameraFollow : MonoBehaviour
         bounds.Encapsulate(PlayerTwo.position);
         
         // Moving camera with smooth
-        transform.position = Vector3.SmoothDamp(transform.position, bounds.center + Offset, ref velocity, SmoothAmount);
+        CameraTransform.position = Vector3.SmoothDamp(transform.position, bounds.center + Offset, ref velocity, SmoothAmount);
         
         // Calculate distance between players
         float distance = Mathf.Sqrt(
             Mathf.Pow(bounds.max.x - bounds.min.x, 2) +
-            Mathf.Pow(bounds.max.z - bounds.min.z, 2));
+            Mathf.Pow(bounds.max.z - bounds.min.z, 2)
+            );
         
         // Zooming the camera dependent on distance
         camera.fieldOfView = Mathf.Lerp(MinZoom, MaxZoom, distance / ZoomLimiter);
-        
-        // Debug.Log("distance : " + distance); // Testing max distance purposes
     }
 }
