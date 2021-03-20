f342f1d0135f5d4097626af67c2f76d11c30a3f5
Kroalg <dawidpaweloszek99@gmail.com>
2021-03-20 13:41:39 +0100

    Quick rework of player movement

--- Assets/PlayerMovementNew.cs
+++ Assets/PlayerMovementNew.cs
@@ -1,18 +1,32 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerMovementNew : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public PlayerInput movement;
+    public PlayerInteract interaction;
+    public float speed = 5f;
+    public float gravity = 9.81f;
+
+    private Vector2 movementInput;
+    private Rigidbody rigidbody;
+
+    private void Start()
     {
-        
+        rigidbody = GetComponent<Rigidbody>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void FixedUpdate()
     {
-        
+        movementInput = movement.MovementVector;
+
+        Vector3 m = new Vector3(movementInput.x, 0, movementInput.y);
+
+        rigidbody.velocity = new Vector3(m.x * speed, rigidbody.velocity.y * gravity, m.z * speed) * Time.deltaTime;
+
+        Vector3 r = new Vector3(movementInput.x, 0, movementInput.y) * 100f * Time.deltaTime;
+        this.transform.LookAt(transform.position + r);
     }
 }
