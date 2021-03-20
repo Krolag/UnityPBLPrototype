493370ff1764527fbff8164b374b73fdb0bb9151
Kroalg <dawidpaweloszek99@gmail.com>
2021-03-20 14:30:49 +0100

    Rework boat_rowing scene for new input, fixing bug with occupied seats

--- Assets/Scripts/Interactable/OarInteraction.cs
+++ Assets/Scripts/Interactable/OarInteraction.cs
@@ -5,33 +5,52 @@ using UnityEngine;
 public class OarInteraction : Interactable
 {
     [SerializeField] private bool isLeft;
-    [SerializeField] private bool isUsed;
-    [SerializeField] private Rigidbody boat;
-    [SerializeField] private float force;
-
-    private void Start()
-    {
-        boat = GetComponentInParent<Rigidbody>();   
-    }
+    [SerializeField] private bool isUsedLeft;
+    [SerializeField] private bool isUsedRight;
+    [SerializeField] private Rigidbody rbBoat;
+    [SerializeField] private float speed;
+    public GameObject[] players;
+    public GameObject seat;
 
     public override void Interact()
     {
         if (isLeft)
         {
+            isUsedLeft = true;
             Debug.Log("left");
-            boat.AddForce(new Vector3(-force, 0.0f, force));
-            boat.transform.Rotate(Vector3.up, -2);
         }
-        if (!isLeft)
+        else
         {
+            isUsedRight = true;
             Debug.Log("right");
-            boat.AddForce(new Vector3(force, 0.0f, force));
-            boat.transform.Rotate(Vector3.up, 2);
         }
+    }
+
+    private void Start()
+    {
+        rbBoat = GetComponentInParent<Rigidbody>();   
     }
 
     private void Update()
     {
-        
+        if (isUsedLeft && isLeft)
+        {
+            if (Vector3.Distance(players[0].transform.position, seat.transform.position) < Vector3.Distance(players[1].transform.position, seat.transform.position))
+            {
+                players[0].transform.position = seat.transform.position;
+            }
+            else
+            {
+                players[1].transform.position = seat.transform.position;
+            }
+        }
+
+
+        if (isUsedLeft)
+        {
+            Debug.Log("Zajête kurwa nie widzisz");
+        }
     }
+
+   
 }
